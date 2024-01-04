5c1ddbb88b7722a619def922eb19c359dd1fe6ce
asfaqjani916 <ashfaqjani916@gmail.com>
2024-01-04 16:14:41 +0530

    fixed api errors

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-let lati;
-let long;
 
-const key = "88076336d8b4250e72b8563d6b5aefd1";
 
 
 window.addEventListener("load",()=>{
+    
     if(navigator.geolocation)
     {
-        navigator.geolocation.getCurrentPosition((position)=>{
-            lati = position.coords.latitude;
-            long = position.coords.longitude;
-        });
-         const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=88076336d8b4250e72b8563d6b5aefd1`
-        const fetchWeather =  async()=>{
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=88076336d8b4250e72b8563d6b5aefd1`);
-            const weather = await response.json();
+         let pos;
+        navigator.geolocation.getCurrentPosition((position)=> {getWeather(position.coords.latitude, position.coords.longitude);});
+        
+        function getWeather(lati, long) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=88076336d8b4250e72b8563d6b5aefd1`)
+                .then(response => response.json())
+                .then(result => {
+                    display(result);
+                })
+                .catch(err => {
+                    alert(err);
+                    console.log(err);
+                });
         }
-
-        console.log(fetchWeather());
+        
     }
 })
+
+function display(result){
+
+}
