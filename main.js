7e5fca1e0d7db0dcbecd50fd3f4d84482a02402a
asfaqjani916 <ashfaqjani916@gmail.com>
2024-01-04 19:41:14 +0530

    changed image paths

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const screen = document.querySelector("body");
 
 
 window.addEventListener("load",()=>{
@@ -23,6 +23,50 @@ window.addEventListener("load",()=>{
     }
 })
 
+
 function display(result){
+
+    console.log(result);
+    var date = new Date();   var time  = date.getHours();
+    if( time > 18)
+    {
+
+        if(result.weather[0].main ==="Rain")
+        {   
+            console.log("its raining")
+        }
+        else{
+        if(result.visibility<500)
+        {
+            console.log("foggy")
+        }
+        else if(result.clouds.all > 50){
+           
+        }
+        else{
+            console.log("clear night sky");
+            screen.style.backgroundImage = url("https://github.com/ashfaqjani916/Weather-App/assets/clear night sky.jpg");
+        }
+    }
+    }
+    else
+    {
+        if(result.weather[0].main ==="Rain")
+        {   
+            console.log("its raining")
+        }
+        else{
+            if(result.visibility<500)
+            {
+                console.log("foggy")
+            }
+            else if(result.clouds.all > 50){
+            console.log("cloudy day");
+            }
+            else{
+            console.log("sunny day");
+        }
+    }
+    }
 
 }
